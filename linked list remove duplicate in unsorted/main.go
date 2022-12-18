83e9963e9e92c8da65b744560b6def41385b9909
nitin.srivastav <nitin.srivastav@t-systems.com>
2022-12-18 17:19:19 +0530

    vowel and constant program

--- linked list remove duplicate in unsorted/main.go
+++ linked list remove duplicate in unsorted/main.go
@@ -1,24 +1,5 @@
 package main
 
-//Count a number of vowels and consonants in a given String?
-//func main() {
-//	str := "india"
-//	countVol := vowel(str)
-//	countCon := len(str) - countVol
-//	fmt.Printf("count of vowel %v and count of consonant : %v", countVol, countCon)
-//
-//}
-//
-//func vowel(str string) int {
-//	count := 0
-//	for _, i2 := range str {
-//		if i2 == 'a' || i2 == 'e' || i2 == 'i' || i2 == 'o' || i2 == 'u' {
-//			count++
-//		}
-//	}
-//	return count
-//}
-
 import "fmt"
 
 type Linkedlist struct {
